c3850fdedbd74e3beb212cb835904b031079aa52
Yasin Silavi <ys.silavi@gmail.com>
2024-03-09 21:53:28 +0330

    fix: wrap `'` character in jsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 function Home() {
   return (
     <section className="home">
-      <p style={{ lineHeight: '160%' }}>
+      <p style={{ lineHeight: "160%" }}>
         {`I'm Yasin Silavi, just a random programmer on the vast expanse of the
         internet, armed with a keyboard and a head full of weird ideas. Whether
         I'm diving into the latest tech trends, hacking away at code, or
@@ -16,8 +16,9 @@ function Home() {
       </p>
 
       <div style={{ marginTop: 900 }}>
-        This is about you, not the others.<br />
-        Don't compare your first with others hundreds
+        This is about you, not the others.
+        <br />
+        {"Don't compare your first with others hundreds"}
       </div>
     </section>
   );
